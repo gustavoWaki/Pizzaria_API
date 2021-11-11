ec937c3d846404f261c4f3fe59491ff265268aae
lirio-do-mato <57483891+lirio-do-mato@users.noreply.github.com>
2021-11-11 09:43:02 -0300

    Add files via upload

---
+++ Cliente Android/java/com/example/pizzariadomario/InicioGerenteActivity.java
@@ -0,0 +1,14 @@
+package com.example.pizzariadomario;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InicioGerenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicial_gerente);
+    }
+}
